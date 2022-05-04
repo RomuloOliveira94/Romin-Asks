fc74ba71aa7c562d4f4a7f1b4a726102212a1ec2
Rômulo Oliveira <romuloffall@gmail.com>
2022-05-03 22:08:46 -0300

    initial commit

--- database/database.js
+++ database/database.js
@@ -1,7 +1,8 @@
 const Sequelize = require("sequelize");
+require("dotenv").config();
 
 //connection with databases
-const connection = new Sequelize("asks", "root", "M@tador123", {
+const connection = new Sequelize("asks", "root", process.env.SQL_PASSWORD, {
   host: "localhost",
   dialect: "mysql",
 });
